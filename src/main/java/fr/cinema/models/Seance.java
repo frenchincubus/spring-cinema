585318f3e3563d9748fbda74e1669fe0bd32aef5
frenchincubus <lionelrenier82@gmail.com>
2021-01-30 17:39:04 +0100

    #3 Seance Map convertie en <String, Film>

--- src/main/java/fr/cinema/models/Seance.java
+++ src/main/java/fr/cinema/models/Seance.java
@@ -1,13 +1,10 @@
 package fr.cinema.models;
 
-import java.time.LocalDateTime;
 import java.util.Map;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
-
-import com.fasterxml.jackson.annotation.JsonFormat;
 
 import lombok.Data;
 
@@ -19,6 +16,6 @@ public class Seance {
 	private String id;
 	private String date;
 	@DBRef
-	private Map<Salle, Film> films;
+	private Map<String, Film> films;
 	
 }
